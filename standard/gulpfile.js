d1ea962ab3e8e110a3b9b931637ce9e159f34890
Matt Urenovich <matt@empoweredmediagroup.com>
2015-12-11 01:35:19 -0500

    Updated standard/gulpfile

--- standard/gulpfile.js
+++ standard/gulpfile.js
@@ -1,6 +1,28 @@
+var $ = require('gulp-load-plugins')();
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 
-gulp.task('default', function() {
-  // place code for default task here
-  console.log('Default Task Fired');
+// Serve Task with browserSync
+gulp.task('serve', ['sass'], function() {
+
+  browserSync.init({
+    server:  "./",
+    port: 7510, // Custom Port #
+    ui: {
+      port: 7511 // Custom UI Port
+    }
+  });
+  gulp.watch('./scss/*.scss', ['sass']);
+  gulp.watch('./*.html').on('change', browserSync.reload);
 });
+
+// Sass w/ browserSync
+gulp.task('sass', function() {
+  return gulp.src('./scss/*.scss')
+  .pipe($.sass());
+  .pipe(gulp.dest('./css'))
+  .pipe(browserSync.stream());
+});
+
+// Default Task
+gulp.task('default', ['serve']);
